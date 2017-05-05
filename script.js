5f4a70aca5bd070ee9f5f60a792eaca535fed594
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-05-05 08:51:58 -0300

    AInda em endamento

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-		
-})
+	//Coloca espaco
+	$("table").css({"border-collapse": "separate", "border-spacing": "10px"})
+	//Seta cor de tudo pra cinza
+	$("td").css({"height": "100px", "width": "100px"});
+
+	//Jogo em si
+	$("td").click(function() {
+		var classe = $(this).attr("class").split(' ')[1];
+
+		$(this).css({"background-color": classe, "transition": "0.5s"});
+	
+		$(this).toggleClass("ativo");
+		$(this).toggleClass("inativo");
+	});
+});
