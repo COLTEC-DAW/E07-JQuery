1b7e6e4cbea3e62475876749d053f06b8a445e1e
mathias goncalves oliveira <a2016952827@la0012217531.infocoltec.local>
2018-05-24 11:11:59 -0300

    versão semi final

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-let pedras = document.getElementsByClassName("pedra")
-let combinations = ["A", "A","B", "B"]
-let current =0;
-let aberto = [];
+let abertos = []
 
 $("document").ready(function(){
-    for(let i = 0;i<pedras.length;i++){
-        $(pedras[i]).css("background-color","yellow");
-        $(pedras[i]).css("height","300px");
-        $(pedras[i]).css("width","300px");
-        pedras[i].estado = false;
-        pedras[i].value = combinations[i];
-        pedras[i].solved = true;
-    }
-    for(let i = 0;i<pedras.length;i++){
-        $(pedras[i]).click(function(){
-            if(this.estado==false){
-                $(this).css("background-color","blue");
-                this.estado = true
-                aberto.push(this)
-                if(aberto.length==2){
-                    for(pedra of aberto){
-                        $(this).css("background-color","yellow");
-                    }
-                    aberto = []
-                }
-            } else {
-                
-                $(this).css("background-color","yellow");
-                this.estado = false
+    $(".pedra").click(function(){
+        if($(this).attr("estado")=="0"){
+            $(this).attr("estado", "1")
+            $(this).css("background-color", $(this).attr("elemento"))
+            abertos.push(this);
+        } else{
+            $(this).attr("estado", "0")
+            $(this).css("background-color", "blue")
+            let index = abertos.indexOf(this)
+            if(index>-1){
+                abertos.splice(index,1)
             }
-        })
-    }
+        }
+        if(abertos.length == 2){
+            setTimeout(() => {
+                if($(abertos[0]).attr("elemento") === $(abertos[1]).attr("elemento")){
+                    $(abertos[0]).attr("estado", "2")
+                    $(abertos[0]).css("background-color", $(abertos[0]).attr("elemento"))
+                    $(abertos[1]).attr("estado", "2")
+                    $(abertos[1]).css("background-color", $(abertos[0]).attr("elemento"))
+                    $(abertos[0]).off()
+                    $(abertos[1]).off()
+                } else{
+                    $(abertos[0]).attr("estado", "0")
+                    $(abertos[0]).css("background-color", "blue")
+                    $(abertos[1]).attr("estado", "0")
+                    $(abertos[1]).css("background-color", "blue")
+                }
+                abertos.splice(1,1)
+                abertos.splice(0,1)
+            },500);
+        }
+        console.log(abertos)
+    })
 })
