b3dfced401653751a0882cb40f0bb44f47861217
erikneves04 <erikrrn04@gmail.com>
2021-07-16 16:09:37 -0300

    Versão 2.0

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 // Acesso aos elementos do HTML
-var ImagesTag = document.body.getElementsByTagName("img");
-var CardsTag = document.body.getElementsByClassName("card");
-var PontosTXT = document.getElementById("Pontos");
-var ErrosTXT = document.getElementById("Erros");
+const ImagesTag = document.body.getElementsByTagName("img");
+const CardsTag = document.body.getElementsByClassName("card");
+const PontosTXT = document.getElementById("Pontos");
+const ErrosTXT = document.getElementById("Erros");
 
 // Nome & caminho padrão das imagens
-var Images = ["csharp.png","css.png","html.png","java.png","mysql.png","php.png","python.png","js.png"];
-var DefaultPath = "./Icones/Icon_";
+const Images = ["csharp.png","css.png","html.png","java.png","mysql.png","php.png","python.png","js.png"];
+const DefaultPath = "./Icones/Icon_";
+
+// Configuração dos temporizadores
+const ImagensDisponiveis = 3800; // milisegundos
+const TempoRestart = 1200;       // milisegundos
 
 // Configurações do jogo
 var Pontos = 0;
 var Erros = 0;
-
 var EmCurso = false;
+var ClickBlock = false;
 
 var Positions = null;
 var Selected_01 = { card: null, img: null, imgIndex: -1 };
 var Selected_02 = { card: null, img: null, imgIndex: -1 };
 var ParEncontrado = [false,false,false,false,false,false,false,false];
+
+// Restaurar variaveis
+function RestartData(){
+    Pontos = 0;
+    Erros = 0;
+    UpdatePoints();
+    EmCurso = false;
+    ClickBlock = false;
+
+    Positions = null;
+    ClearSelected();
+    ParEncontrado = [false,false,false,false,false,false,false,false];
+}
 
 // Definição randomica da posição de cada imagem no tabuleiro
 function DefinePositions(){
@@ -43,8 +60,8 @@ function DefinePositions(){
 // Exibição de todas as imagens no mesmo instante
 function ShowAllImages(){
     for(var i=0;i<16;i++) {
-        CardsTag[i].style.backgroundColor = "#FFFFFF";
         ImagesTag[i].src = DefaultPath + Images[Positions[i]]; 
+        //CardsTag[i].style.backgroundColor = "#FFFFFF";
     } 
 }
 
@@ -52,14 +69,14 @@ function ShowAllImages(){
 function HideAllImages(){ 
     for(var i=0;i<16;i++) {
         ImagesTag[i].src = "";
-        CardsTag[i].style.backgroundColor = "#F1C4FF"
+        //CardsTag[i].style.backgroundColor = "#F1C4FF"
     }
 }
 
 // Inicia o jogo e realiza as configurações iniciais
 function StartGame(){
     if(EmCurso){
-        alert("O Jogo ja foi iniciado!\nUtilize o botão Reniciar!");
+        alert("O Jogo ja foi iniciado!\nUtilize o botão Reiniciar!");
         return;
     }
 
@@ -67,36 +84,73 @@ function StartGame(){
     Positions = DefinePositions();
 
     ShowAllImages();
-    setTimeout(HideAllImages,1300);
+    setTimeout(HideAllImages,ImagensDisponiveis);
 }
 
 // Reinicia o jogo e as configurações iniciais
 function RestarGame(){
-    Positions = DefinePositions();;
-    Selected_01 = { card: null, img: null, imgIndex: -1 };
-    Selected_02 = { card: null, img: null, imgIndex: -1 };
-    ParEncontrado = [false,false,false,false,false,false,false,false];
-
-    Pontos = 0; PontosTXT.textContent = "Pontuação: " + Pontos;
-    Erros = 0;  ErrosTXT.textContent = "Erros: " + Erros;
+    RestartData();
 
     HideAllImages();
-    setTimeout(ShowAllImages,800);
-    setTimeout(HideAllImages,2100);    
+    setTimeout(StartGame,TempoRestart);    
 }
 
+// Clique em um card
 function CardClick(card){
-    var index = -1;
 
+    if(Selected_01.card == card && Selected_01.card != null) return;
+    if(ClickBlock) return;
+    if(!EmCurso){
+        alert("Aperte o botão iniciar primeiro!");
+        return;
+    }
+    
+    var index = -1;
     for(var i=0;i<16;i++) if(card === CardsTag[i]) index = i;
     if(index == -1) return;
 
-    if(ParEncontrado[Positions[index]]) {
-        console.log("Meti o pé");
-        return;
+    if(ParEncontrado[Positions[index]]) return; 
+
+    Select(card,index);
+
+    if(Selected_02.card != null) CheckPair();
+
+}
+
+// Confere o par de cartões
+function CheckPair(){
+    if(Selected_01.img.src == Selected_02.img.src){
+        Scored();
+    }else if(Selected_01.card != null && Selected_02.card != null){
+        Failed(index); 
     }
 
-    ImagesTag[index].src = DefaultPath + Images[Positions[index]]; 
+    UpdatePoints();
+
+    if(ParEncontrado.indexOf(false) == -1){
+        setTimeout(function(){
+            alert("Você venceu");
+        },100);
+    }
+
+}
+
+// Atualização da pontuação
+function UpdatePoints(){
+    PontosTXT.textContent = "Pontuação: " + Pontos;
+    ErrosTXT.textContent = "Erros: " + Erros;    
+}
+
+// Limpar os cards selecionados
+function ClearSelected(){
+    Selected_01.card = null; Selected_01.img = null; Selected_01.imgIndex = -1;
+    Selected_02.card = null; Selected_02.img = null; Selected_02.imgIndex = -1;
+}
+
+// Seleciona um card
+function Select(card, index){
+    ImagesTag[index].src = DefaultPath + Images[Positions[index]];
+    //CardsTag[index].style.backgroundColor = "#FFFFFF";
 
     if(Selected_01.card == null){
         Selected_01.card = card;
@@ -107,33 +161,31 @@ function CardClick(card){
         Selected_02.img = ImagesTag[index];
         Selected_02.imgIndex = Positions[index];
     }
-
-    if(Selected_01.card == null || Selected_02.card == null){
-    }else if(Selected_01.img.src == Selected_02.img.src){
-        ParEncontrado[Selected_01.imgIndex] = true;
-
-        Selected_01.card = null; Selected_01.img = ""; Selected_01.imgIndex = -1;
-        Selected_02.card = null; Selected_02.img = ""; Selected_02.imgIndex = -1;
-
-        Pontos++;
-        PontosTXT.textContent = "Pontuação: " + Pontos;
-    }else if(Selected_01.card != null && Selected_02.card != null){  
-        setTimeout(function(){
-            ImagesTag[index].src = "";
-            for(var i=0;i<16;i++) if(card === CardsTag[i]) index = i;
-
-            Selected_01.img.src = ""; Selected_01.card = null; Selected_01.img = null; Selected_01.imgIndex = -1;
-            Selected_02.img.src = ""; Selected_02.card = null; Selected_02.img = null; Selected_02.imgIndex = -1;
-        },800);
-        
-        Erros++;
-        ErrosTXT.textContent = "Erros: " + Erros;
-    }
-    
-    if(ParEncontrado.indexOf(false) == -1){
-        setTimeout(function(){
-            alert("Você venceu");
-        },100);
-    }
 }
 
+// Combinação correta de cards
+function Scored(){
+    ParEncontrado[Selected_01.imgIndex] = true;
+    Pontos++;
+    ClearSelected();
+}
+
+// Combinação incorreta de cards
+function Failed(index){
+    ClickBlock = true;  
+    setTimeout(function(){
+        ImagesTag[index].src = "";
+
+        if(Selected_01.img != null) {
+            Selected_01.img.src = "";
+            Selected_02.img.src = "";
+            //CardsTag[Selected_01.imgIndex].style.backgroundColor = "#F1C4FF";
+            //CardsTag[Selected_02.imgIndex].style.backgroundColor = "#F1C4FF";
+            ClearSelected();
+        }
+
+        ClickBlock = false;
+    },TempoRestart);
+        
+    Erros++;
+}
