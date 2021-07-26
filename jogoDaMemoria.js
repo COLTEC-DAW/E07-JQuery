efe1b4331b4666dd96986b4fcce5f00f6b5fee5a
BernardoVenancio <bernardovenancio1@gmail.com>
2021-07-25 21:45:59 -0300

    Adição de Funcionalidades

--- jogoDaMemoria.js
+++ jogoDaMemoria.js
@@ -3,7 +3,13 @@ $(document).ready(()=> {
     $(".cards").flip();
 })
 
+//Variáveis
 var count = 0;
+var cardPair = []
+var teste = [];
+var teste1 = [];
+
+//Função para contar numero de cartas viradas
 const contar = () => {
     count++;
     if(count > 2){
@@ -14,13 +20,40 @@ const contar = () => {
         alert(count);
     }
 }
+
+const check = () =>{
+    if(count == 2){
+        if(cardPair[0].querySelector('.back>img').src == cardPair[1].querySelector('.back>img').src ){
+            $(cardPair[0]).off(".flip");
+            $(cardPair[1]).off(".flip");
+            count = 0;
+            cardPair = [];
+        }else{
+            console.log("erro")
+            count = 0;
+            $(cardPair[0]).flip(false);
+            $(cardPair[1]).flip(false);
+            cardPair = [];
+        }
+    }
+}
+
+function acerto(){
+
+
+}
+
+//Função que faz uma ação caso a carta seja virada
 $(".cards").on('flip:done',function(){
     var flip = $(this).data("flip-model");
     if(flip.isFlipped){
-        contar();
+        count++;
+        cardPair.push(this);
+        check();
     }
 })
 
+//Embaralha as cartas
 function embaralhar(){
     var images = $(".back>img");
     var sources = ['./Memoria/Ainz.png','./Memoria/Albedo.png','./Memoria/Aqua.png','./Memoria/Emilia.png','./Memoria/Filo.png','./Memoria/Ram.jpg','./Memoria/Raphtalia.png','./Memoria/Rem.png','./Memoria/Ainz.png','./Memoria/Albedo.png','./Memoria/Aqua.png','./Memoria/Emilia.png','./Memoria/Filo.png','./Memoria/Ram.jpg','./Memoria/Raphtalia.png','./Memoria/Rem.png']
