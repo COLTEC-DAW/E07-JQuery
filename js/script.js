8b4abe5ac71500fdc7f545a3f205a41c3cee10c9
CORDERIN <102868783+CORDERIN@users.noreply.github.com>
2022-06-29 20:39:01 -0300

    +

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
 
+    let imgs = [];
+
     for(let i = 0; i < 16; i++){
 
+        let img ={
+            src: "../img/" + i + ".jpg",
+            id: i%8
+        };
+
+        imgs.push(img);
        
         $("#card_" + i).click(function(){
-                const back = 0;
-                const front = 1;
+    
+                const back = 0, front = 1;
                 let faces = this.getElementsByClassName("face");
+                
                 faces[back].classList.toggle("flipped");
                 faces[front].classList.toggle("flipped");
-            
+                faces[front].style.background = "url('"+ imgs[i].src +"')";
+                faces[front].style.backgroundSize = "cover";
             });
         }
 
@@ -17,4 +27,4 @@ $(document).ready(function(){
 
     
 
-
+ 
