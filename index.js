07d98257f03dc22c8963401f46da421608f8ec8a
JeanBerly <jeanberlyy@gmail.com>
2022-06-30 11:33:59 -0300

    feat: Agora o jogo mostra quando o player ganhou e com quantas tentativas, além de oferecer a opção de jogar novamente

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let cardSelected = false;
 let firstCardSelected = null;
 let secondCardSelected = null;
 let isExecuting = false;
+let numberOfAttempts = 0;
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -24,46 +25,67 @@ function drawCards(cards){
     return drewCards;
 }
 function paintCards(drewCards){
+    // copiando por valor e não referência para poder jogar mais de uma vez!
+    let possibleColors = [...colors];
     for(i = 0; i < drewCards.length; i += 2){
-        let indexOfRandomColor = getRandomInteger(0, colors.length);
-        let randomColor = colors[indexOfRandomColor];
-        colors.splice(indexOfRandomColor, 1);
+        let indexOfRandomColor = getRandomInteger(0, possibleColors.length);
+        let randomColor = possibleColors[indexOfRandomColor];
+        possibleColors.splice(indexOfRandomColor, 1);
         $("#" + drewCards[i].id).addClass(randomColor);
         $("#" + drewCards[i+1].id).addClass(randomColor);
     }
 }
-function makeCardsBlack(cards){     
-    cards.map((_, card) =>{
-        $("#" + card.id).addClass("black");
-    })
+function makeCardsBlack(cards){
+    for (i = 0; i < cards.length; i++) {
+        $("#" + cards[i].id).addClass("black");
+    }
+}
+function resetCards(cards){
+    for (i = 0; i < cards.length; i++){
+        card = $("#" + cards[i].id);
+        card.removeClass(card.attr("class").split(" ")[1]);
+    }
 }
 function initGame(){
+    numberOfAttempts = 0;
+    resetCards(getAllCards());
     drawCards(getAllCards());
     makeCardsBlack(getAllCards());
-    $(".card").click(this, handleCardClick);
+    getAllCards().click(this, handleCardClick);
 }
 async function handleCardClick(card){
-    if (isExecuting) return
+    if (isExecuting) return;
     card = $("#" + card.target.id)[0]
     if (!cardSelected){
         firstCardSelected = card;
         $(firstCardSelected).removeClass("black");
-        cardSelected = !cardSelected;
-        return;
     }
-    isExecuting = true;
-    secondCardSelected = card;
-    $(secondCardSelected).removeClass("black");
-    await new Promise(r => setTimeout(r, 1000));
-    isExecuting = false;
-    let colorOfFirstCard = $(firstCardSelected).attr("class").split(" ")[1];
-    let colorOfSecondCard = $(secondCardSelected).attr("class").split(" ")[1];
-    if (colorOfFirstCard == colorOfSecondCard && !cardsEqual(firstCardSelected, secondCardSelected)){
-        cardSelected = !cardSelected;
-        return;
+    else{
+        secondCardSelected = card;
+        isExecuting = true;
+        $(secondCardSelected).removeClass("black");
+        // Para ter um tempo do jogador ver a dupla de cartas
+        await new Promise(r => setTimeout(r, 1000));
+        isExecuting = false;
+        let colorOfFirstCard = $(firstCardSelected).attr("class").split(" ")[1];
+        let colorOfSecondCard = $(secondCardSelected).attr("class").split(" ")[1];
+        if (colorOfFirstCard == colorOfSecondCard && !cardsEqual(firstCardSelected, secondCardSelected)) {
+            // Quando o player acerta a dupla de cartas elas não precisam mais do eventListener de click!
+            $(firstCardSelected).unbind();
+            $(secondCardSelected).unbind();
+            if ($(".black").length == 0) {
+                alert(`"Parabéns, você ganhou com ${numberOfAttempts} palpites!!!"`)
+                if (confirm('Você deseja jogar novamente? Se sim, clique em OK.')) {
+                    initGame()
+                }
+            }
+        }
+        else{
+            $(firstCardSelected).addClass("black");
+            $(secondCardSelected).addClass("black");
+        }
+        numberOfAttempts++;
     }
-    $(firstCardSelected).addClass("black");
-    $(secondCardSelected).addClass("black");
     cardSelected = !cardSelected;
 }
 function cardsEqual(card1, card2){
