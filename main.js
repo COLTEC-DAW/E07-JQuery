0950ece53a39c821d9efc4845527822596ff0190
Bigodeiro <enzobigber@gmail.com>
2022-07-01 14:50:12 -0300

    As cartas estão virando

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-$("#myBtn").click(function(){
-    $("p").hide();
-});
+const cartas = document.querySelectorAll('.card');
+
+function viraCarta()
+{
+    this.classList.toggle('virada');
+}
+
+cartas.forEach(card => {card.addEventListener('click', viraCarta)});
