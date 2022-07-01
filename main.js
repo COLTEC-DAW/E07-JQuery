0979422c9d794eb0913fcbf86786d8364eec805c
Bigodeiro <enzobigber@gmail.com>
2022-07-01 16:06:40 -0300

    So falta embaralhar

--- main.js
+++ main.js
@@ -1,8 +1,71 @@
 const cartas = document.querySelectorAll('.card');
 
+let cartasSeleccionadas = [];
+let virouCarta = false;
+let primeiraCarta, segundaCarta;
+let trava = false;
+
+var audio_acertou = new Audio('smb_coin.wav');
+var audio_errou = new Audio('smb_bump.wav');
+
+(function embaralha()
+{
+    cartas.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.style.order = randomPos;
+    });
+})();
+
+function comparaCartas()
+{
+    if (primeiraCarta.dataset.framework === segundaCarta.dataset.framework)
+    {
+        desabilitaCartas();
+        audio_acertou.play();
+    }
+    else
+    {
+        desviraCartas();
+        audio_errou.play();
+    }
+}
+
+function desabilitaCartas()
+{
+    primeiraCarta.removeEventListener('click', viraCarta);
+    segundaCarta.removeEventListener('click', viraCarta);
+}
+
+function desviraCartas()
+{
+    trava = true;
+
+    setTimeout(() => {
+        primeiraCarta.classList.remove('virada');
+        segundaCarta.classList.remove('virada');
+        trava = false;
+    }, 500);
+}
+
 function viraCarta()
 {
-    this.classList.toggle('virada');
+    if (trava) return
+    if (this === primeiraCarta) return;
+
+    this.classList.add('virada');
+
+    if(!virouCarta)
+    {
+        primeiraCarta = this;
+        virouCarta = true;
+    }else
+    {
+        segundaCarta = this;
+        virouCarta = false;
+
+        comparaCartas();
+    }
 }
+
 
 cartas.forEach(card => {card.addEventListener('click', viraCarta)});
