6e82763edcb50468a37f825cea7e0cced5aa542b
Amanda Bueno <amandabuenocampos@gmail.com>
2022-07-04 12:46:56 -0300

    entrega trabalho -todo's

--- memoryGame.js
+++ memoryGame.js
@@ -4,63 +4,75 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
-function shuffle() {
-    cards.forEach(card => {
-      let randomPos = Math.floor(Math.random() * cards.length);
-      card.style.order = randomPos;
-    });
-    
-};
-
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+    if (lockBoard || this === firstCard) return;
+    console.log(this);
+    this.classList.add('flip');
 
-  this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = { cardObject: this, cardClass: $(this).attr("class").split(' ') };
+        console.log(this);
+        console.log(firstCard);
+        return;
+    }
 
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-    return;
-  }
-  secondCard = this;
-  checkForMatch();
+    secondCard = { cardObject: this, cardClass: $(this).attr("class").split(' ') };
+    console.log(this);
+    console.log(firstCard);
+    console.log(secondCard);
+
+    checkForMatch();
 }
 
 function checkForMatch() {
-  firstCard.dataset.framework === secondCard.dataset.framework ? disableCards() : unflipCards();
+    (firstCard.cardClass[firstCard.cardClass.length - 2] == secondCard.cardClass[secondCard.cardClass.length - 2]) ? disableCards() : unflipCards();
 }
 
 function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-  resetBoard();
+    console.log("is a match")
+    firstCard.cardObject.removeEventListener('click', flipCard);
+    secondCard.cardObject.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
-  lockBoard = true;
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-    resetBoard();
-  }, 1500);
+    console.log("not a match")
+    lockBoard = true;
+
+    setTimeout(() => {
+        console.log("time out");
+        console.log(firstCard);
+        console.log(secondCard);
+        firstCard.cardObject.classList.remove('flip');
+        secondCard.cardObject.classList.remove('flip');
+
+        resetBoard();
+    }, 1000);
 }
 
 function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+    console.log("reseting this shit")
+    hasFlippedCard = lockBoard = false;
+    firstCard = secondCard = null;
 }
 
-function memoryGame(){
+
+function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * cards.length);
+        card.style.order = randomPos;
+    });
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+function memoryGame() {
     shuffle();
-    cards.forEach(card => card.addEventListener('click', flipCard));
 }
 
 memoryGame();
 
-// todo
-// corrigir o bug do grid que segue ficando errado :p
-// voltar as cartas para a posição correta após cada jogada
-// colocar um timer
-// mensagem de sucesso e de erro p o usuário
-// fazer um shuffle de row e não só de col 
+//todo
+// add a timer and a win message
