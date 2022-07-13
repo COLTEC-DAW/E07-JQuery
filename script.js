d4fdf739e074c6eadd3b874f49e38cb8ce2c2ff0
wesleyfig <2019951937@teiacoltec.org>
2022-07-13 12:13:54 -0300

    jogo funcionando

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const imgs = [
     'fotoP(6).jpg',
 ];
 
+imgs = randomImgs(imgs);
+
 let cardHTML = '';
 
 imgs.forEach(img => 
@@ -26,10 +28,45 @@ cardBoard.innerHTML = cardHTML + cardHTML;
 
 const cards = document.querySelectorAll(".memory-card");
 var numberOfClicks = 0;
-var gameWon = false;
+var numberOfHits = 0;
+
 function flipCard()
 {
     this.classList.add("flip");
+}
+
+function unflipCards()
+{
+    $(".first-card").removeClass("flip");   
+    $(".second-card").removeClass("flip"); 
+}
+
+function disableCards()
+{
+    $(".first-card").off("click");
+    $(".second-card").off("click");
+}
+
+function computeHits()
+{
+    $(".first-card").toggleClass("hit");
+    $(".second-card").toggleClass("hit");
+    numberOfHits++;
+}
+
+function removeClasses()
+{
+    $(".first-card").removeClass("first-card");
+    $(".second-card").removeClass("second-card");
+}
+
+function finishGame()
+{
+    setTimeout(() => 
+    {
+        alert("Parabêns, você ganhou!");
+        window.location.reload();
+    }, 500);
 }
 
 $(document).ready(function()
@@ -37,23 +74,36 @@ $(document).ready(function()
     $(".memory-card").click(function()
     {
         numberOfClicks++;
-            if(numberOfClicks % 2 != 0)
+        if(numberOfClicks % 2 != 0)
+        {
+            $(this).toggleClass("first-card");
+        } else {
+            $(this).toggleClass("second-card");
+
+            // get images src
+            var node1 = document.getElementsByClassName("memory-card flip first-card");
+            var firstCard = node1[0].children[0].src;
+            var node2 = document.getElementsByClassName("memory-card flip second-card");
+            var secondCard = node2[0].children[0].src;  
+
+            if(firstCard === secondCard)
             {
-                $(this).toggleClass("first-card");
-            } else
-            {
-                $(this).toggleClass("second-card");
-                var node1 = document.getElementsByClassName("memory-card flip first-card");
-                var firstCard = node1[0].children[0].src;
-                var node2 = document.getElementsByClassName("memory-card flip second-card");
-                var secondCard = node2[0].children[0].src;  
-                if(firstCard === secondCard)
+                computeHits();
+                disableCards();
+                removeClasses();
+            }  else {
+                setTimeout(() => 
                 {
-                    var correct = true;
-                } else {
-                }        
-            }                    
-        }    
+                    unflipCards();
+                    removeClasses();
+                }, 500);
+            };        
+            if(numberOfHits == 6)
+            {   
+                finishGame();
+            }
+        }  
+    }    
     );
 });
 
